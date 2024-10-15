c869117df46f4f4f3db829c1af8f04dc9f5d9c61
MechaMask <vesper.ludwig@outlook.com>
2024-10-15 15:42:32 -0400

    added updatable and drawable containers

--- main.py
+++ main.py
@@ -8,17 +8,26 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     delta = 0
     clock =  pygame.time.Clock()
-    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
+    
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)  
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(delta)
+        #update objects
+        for obj in updatable:
+            obj.update(delta)
 
         #rendering
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
+        
         delta = clock.tick(60)/1000 #update delta time
         
 
