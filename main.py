fc6f620e03dea171720b4e63fa17b7deed726b48
MechaMask <vesper.ludwig@outlook.com>
2024-10-15 14:09:35 -0400

    frame rate

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from constants import *
 
 def main():
     print("Starting asteroids!")
-    pg = pygame.init()
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    delta = 0
+    clock =  pygame.time.Clock()
+    
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        delta = clock.tick(60)/1000
+
 if __name__ == "__main__":
     main()
