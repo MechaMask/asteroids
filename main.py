fd3f07c09d01ed01ed16e8dc477233fec54683d4
MechaMask <vesper.ludwig@outlook.com>
2024-10-15 15:05:07 -0400

    added player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
@@ -7,14 +8,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     delta = 0
     clock =  pygame.time.Clock()
-    
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)    
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         delta = clock.tick(60)/1000
+        
 
 if __name__ == "__main__":
     main()
